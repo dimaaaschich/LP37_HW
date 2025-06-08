bd5a8ac130f5426edfa8bd2ad501c46cfb658a92
Dmitriy Shishko <dimaaaschich@gmail.com>
2025-06-08 19:31:56 +0300

    bot code

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 from telegram.ext import Updater
 
 def main():
-    LP37_chich_bot = Updater('7671951321:AAEed-2Y6wucqFDGfXFvVG4-YCZQygZOWoo', use_context=True)
-    LP37_chich_bot.start_polling()
-    LP37_chich_bot.idle()
+    mybot = Updater("7671951321:AAEed-2Y6wucqFDGfXFvVG4-YCZQygZOWoo", use_context=True)
+
+    mybot.start_polling()
+    mybot.idle()
 
 main()
