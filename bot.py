2ad54c44032e0af2d2130f8d3e4e3d914d3b4d64
Dmitriy Shishko <dimaaaschich@gmail.com>
2025-06-09 16:04:37 +0300

    bot file delited

--- bot.py
+++
@@ -1,9 +0,0 @@
-from telegram.ext import Updater
-
-def main():
-    mybot = Updater("7671951321:AAEed-2Y6wucqFDGfXFvVG4-YCZQygZOWoo", use_context=True)
-
-    mybot.start_polling()
-    mybot.idle()
-
-main()
